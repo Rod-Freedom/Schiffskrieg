2ba9697d9947da6c07210a7a57e245389c1a878a
David Miranda <dvdfermiranda@yahoo.com.mx>
2024-07-04 20:40:13 -0600

    Added login and signup buttons

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,36 +1,37 @@
 const signUpFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password').value.trim();
+  event.preventDefault();
 
-    const passwordConfirm = document.querySelector('#confirm-password').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
 
-    const nickname = document.querySelector('#username').value.trim();
-  
-    if ((nickname && email && password && passwordConfirm) && (password === passwordConfirm)) {
-      try {
-        const response = await fetch('/api/players/signup', {
-          method: 'POST',
-          body: JSON.stringify({ nickname, email, password}),
-          headers: { 'Content-Type': 'application/json' },
-        });
-      
-        if (response.ok) {
-          alert('')
-          document.location.replace('/');
-        } else {
-          alert(`Error: ${error.message}`);
-        }
+  const passwordConfirm = document.querySelector('#confirm-password').value.trim();
 
-    }catch (error) {
+  const nickname = document.querySelector('#username').value.trim();
+
+  if ((nickname && email && password && passwordConfirm) && (password === passwordConfirm)) {
+    try {
+      const response = await fetch('/api/players/signup', {
+        method: 'POST',
+        body: JSON.stringify({ nickname, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.ok) {
+        alert('')
+        document.location.replace('/');
+      } else {
+        alert(`Error: ${error.message}`);
+      }
+
+    } catch (error) {
       console.error('Error:', error);
     }
   }
 
 }
-  
-  document
-    .querySelector('#signup-form')
-    .addEventListener('submit', signUpFormHandler);
-  
+
+document
+  .querySelector('#signup-form')
+  .addEventListener('submit', signUpFormHandler);
+
+document.querySelector('#login-button').addEventListener('click', () => location.href = "./login");
