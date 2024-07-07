ffd6d63fb3fa5ee1730b03172ebf0491dad60074
David Miranda <dvdfermiranda@yahoo.com.mx>
2024-07-06 18:50:44 -0600

    Added game handlebar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -30,9 +30,7 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/game', withAuth, (req, res) => {
-  res.render('game.handlebars', {
-    logged_in: req.session.logged_in,
-  });
+  res.render('game.handlebars', { layout: false });
 });
 
 router.get('/signup', (req, res) => {
@@ -88,9 +86,9 @@ router.get('/profile', withAuth, async (req, res) => {
       const opponentId = nemesis_id[0].opponent_id;
 
       const nemesis = await Player.findByPk(opponentId);
-      nemesis_id[0].opponent  = nemesis ? nemesis.nickname : null;
+      nemesis_id[0].opponent = nemesis ? nemesis.nickname : null;
     }
- 
+
     // retrieve the coordinate with most hits by all the oponents of the user
     const weakPoint = await Shot.findAll({
       attributes: [
@@ -192,36 +190,36 @@ router.get('/profile', withAuth, async (req, res) => {
         }
       });
 
-       // Compute the number of misses before the first hit for each match
-       let missesBeforeFirstHit = 0;
-       let hitFound = false;
-       for (const shot of shotsInEachMatch) {
-         if (shot.result === 'Hit') {
-           hitFound = true;
-           break;
-         } else if (shot.result === 'Miss') {
-           missesBeforeFirstHit++;
-         }
-       };
- 
-       // If a hit was found, include this match in the average calculation
-       if (hitFound) {
-         totalMisses += missesBeforeFirstHit;
-         matchesWithHits++;
-       };
-     };
+      // Compute the number of misses before the first hit for each match
+      let missesBeforeFirstHit = 0;
+      let hitFound = false;
+      for (const shot of shotsInEachMatch) {
+        if (shot.result === 'Hit') {
+          hitFound = true;
+          break;
+        } else if (shot.result === 'Miss') {
+          missesBeforeFirstHit++;
+        }
+      };
 
-     const avgFailuresBeforeFirstHit = matchesWithHits > 0 ? totalMisses / matchesWithHits : 0;
+      // If a hit was found, include this match in the average calculation
+      if (hitFound) {
+        totalMisses += missesBeforeFirstHit;
+        matchesWithHits++;
+      };
+    };
 
-    res.render('profile.handlebars',{
+    const avgFailuresBeforeFirstHit = matchesWithHits > 0 ? totalMisses / matchesWithHits : 0;
+
+    res.render('profile.handlebars', {
       nickname,
       victoryCount,
       matchesPlayed,
       defeatCount,
-      weakPoint : weakPoint[0].coordinate,
-      nemesis : nemesis_id[0].opponent,
-      avgHitsPerMatch : avgHitsPerMatch[0].hits_per_match,
-      avgFailuresPerMatch : avgMissPerMatch[0].miss_per_match,
+      weakPoint: weakPoint[0].coordinate,
+      nemesis: nemesis_id[0].opponent,
+      avgHitsPerMatch: avgHitsPerMatch[0].hits_per_match,
+      avgFailuresPerMatch: avgMissPerMatch[0].miss_per_match,
       avgFailuresBeforeFirstHit
     });
   } catch (error) {
