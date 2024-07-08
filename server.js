6d4bf21d1da4ed2c258d064cfe6d3d8e2a52b20d
David Miranda <dvdfermiranda@yahoo.com.mx>
2024-07-08 00:25:33 -0600

    Added feature to stop the user from playing against himself

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { Match } = require('./models');
+const { Shots } = require('./models');
 
 // In-game tracker
 const gameParamsObj = {
@@ -18,6 +20,7 @@ const gameTracker = new SocketTracker(gameParamsObj);
 // socket.io packages
 const http = require('http');
 const { Server } = require('socket.io');
+const { match } = require('assert');
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -53,7 +56,7 @@ app.use(routes);
 const io = new Server(httpServer);
 
 //Give io access to session object
-// io.engine.use(sessionMiddleware);
+io.engine.use(sessionMiddleware);
 
 sequelize.sync({ force: false }).then(() => {
   httpServer.listen(PORT, () => {
@@ -61,44 +64,50 @@ sequelize.sync({ force: false }).then(() => {
   });
 });
 
+
 let isGame = false;
-let player1 = { number: 1, connected: false, ready: false };
-let player2 = { number: 2, connected: false, ready: false };
-let players = [player1, player2];
+const player1 = { number: 1, connected: false, ready: false };
+const player2 = { number: 2, connected: false, ready: false };
+const players = [player1, player2];
+
+let matchId;
+let playersIds = [];
 
 io.on('connection', (socket) => {
 
-  // const session = socket.request.session;
-  // console.log(session);
-  const playerNum = 0;
+  let playerNum;
+  const playerId = socket.request.session.player_id;
+
+  for (const id of playersIds) {
+    if (id === playerId) {
+      return;
+    }
+  }
+  playersIds.push(playerId);
+
 
   for (const i in players) {
     if (!players[i].connected) {
       playerConnection(socket, players[i]);
-      playerNum = i + 1;
-    } else {
-      socket.emit('full-server');
+      playerNum = Number(i) + 1;
+      break;
+    } else if (players[0].connected && players[1].connected) {
       console.log('Additional player connection attempted');
       return;
     }
   }
 
-  if (players[0].ready & players[1].ready) {
-    socket.emit('start-game');
-    socket.broadcast('start-game');
-    isGame = true;
-  };
-
   socket.on('disconnect', () => {
-    endGame();
-    console.log(`Player ${playerNum} disconnected.`);
+    if (isGame) endGame();
     players[playerNum - 1].connected = false;
     players[playerNum - 1].ready = false;
+    playersIds.splice(playersIds.indexOf(playerId, 1));
+    console.log(`Player ${playerNum} disconnected.`);
   });
 
   // –––––––––––––––––––––––––––––––––––––––––––
   // in-game funcs
-  socket.on('send-ships', (ships, player) => {
+  socket.on('send-ships', async (ships, player) => {
     if (player === 1) {
       gameTracker.playerOneShips = ships;
       players[0].ready = true;
@@ -106,25 +115,26 @@ io.on('connection', (socket) => {
       gameTracker.playerTwoShips = ships;
       players[1].ready = true;
     }
-    
     if (players[0].ready && players[1].ready) {
+      const match = await Match.create();
+      matchId = match.dataValues.match_id;
       socket.broadcast.emit('init-game', true);
       socket.emit('init-game', true);
       isGame = true;
     }
-  })
+  });
 
-  socket.on('take-shot', coord => console.log(coord, 'hi'))
+  socket.on('take-shot', coord => console.log(coord, 'hi'));
 });
 
 // server funcs
 function playerConnection(socket, player) {
   player.connected = true;
-  console.log(`player ${player.number} conected`);
+  console.log(`player ${player.number} connected`);
   socket.emit('player-number', player.number);
   socket.broadcast.emit('player-connection', player.number);
 };
 
-const endGame = async () {
-  //query databases
+const endGame = async () => {
+
 }
